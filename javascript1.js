88662f1515b405dcd8fd4b6940e235bcc9d26697
Kaushik Raj Ghimire <104378051+kaushikRajGhimire@users.noreply.github.com>
2023-07-15 04:08:26 +0700

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,28 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+var a="kaushik";
+var b="raj";
+var c="ghimire";
+console.log(a+b+c);
+
+var a=[12,13,14];
+var b=a;
+b.pop();
+console.log(a);
+
+if(-1)
+{
+    console.log(2);
+}
+else
+console.log(3);
+
+for(var i=25;i<51;i++)
+console.log(i);
+
+function kaushik()
+{
+    console.log("krg");
+}
+kaushik();
